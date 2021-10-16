f8fc3eaed79259547cc544946f8b1c9209f5e6a4
t0in4 <t0in4@yahoo.com>
2021-10-16 18:40:30 +0300

    In MainActivity.kt 1. Implementation of overriding system back button for double tap back to exit application 2. retrieving string from values in .kt file

--- app/src/main/java/com/eyehail/rubinpazzles/MainActivity.kt
+++ app/src/main/java/com/eyehail/rubinpazzles/MainActivity.kt
@@ -6,8 +6,11 @@ import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var backPressedTime: Long = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,7 +26,26 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }*/
 
-        val w:Window = getWindow()
-        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        val w: Window = getWindow()
+        w.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
     }
+    //Double tap back to exit android - start
+    override fun onBackPressed() {
+
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+
+            super.onBackPressed()
+            return
+        } else {
+            //implement retrieving string from values (for translation in future)
+            Toast.makeText(getBaseContext(), getString(R.string.exitTwice), Toast.LENGTH_SHORT).show()
+        }
+            backPressedTime = System.currentTimeMillis()
+        }
+    //Double tap back to exit android - end
+
+
 }
