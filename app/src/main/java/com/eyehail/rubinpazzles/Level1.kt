029181692e0748c5994c67c1ef9189d96fef70e1
t0in4 <t0in4@yahoo.com>
2021-10-18 08:11:27 +0300

    Creating first Level1.kt 1. adding image_left to universal.xml 2. adding rounding image corners code in Level1.kt

--- app/src/main/java/com/eyehail/rubinpazzles/Level1.kt
+++ app/src/main/java/com/eyehail/rubinpazzles/Level1.kt
@@ -1,9 +1,11 @@
 package com.eyehail.rubinpazzles
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import android.widget.ImageView
 
 class Level1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,12 @@ class Level1 : AppCompatActivity() {
         val w: Window = getWindow()
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         //set screen on whole screen - end
+
+        //making corners round to image_left - start
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            findViewById<ImageView>(R.id.image_left).clipToOutline = true
+        }
+        //making corners round to image_left - end
+
     }
 }
