71b9c8866a2ad3a4f852e923544376f2dba75531
t0in4 <t0in4@yahoo.com>
2021-10-24 16:15:05 +0300

    making left picture clickable

--- app/src/main/java/com/eyehail/rubinpazzles/Level1.kt
+++ app/src/main/java/com/eyehail/rubinpazzles/Level1.kt
@@ -1,5 +1,6 @@
 package com.eyehail.rubinpazzles
 
+import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.Intent
 import android.graphics.Color
@@ -7,6 +8,8 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.view.animation.Animation
@@ -23,6 +26,7 @@ class Level1 : AppCompatActivity() {
     var numRight: Int = 0 //variable for right picture
     var count: Int = 0  //variable for counter
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.universal)
@@ -34,7 +38,10 @@ class Level1 : AppCompatActivity() {
 
         //set screen on whole screen - start
         val w: Window = getWindow()
-        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        w.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         //set screen on whole screen - end
 
         //button to back to main screen - start
@@ -61,7 +68,8 @@ class Level1 : AppCompatActivity() {
         dialog = Dialog(this) //creating new dialog window
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE) //we don't need to use title
         dialog.setContentView(R.layout.previewdialog)// path to dialog window
-        dialog.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) // making dialog window background transparent
+        dialog.getWindow()
+            ?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) // making dialog window background transparent
         dialog.setCancelable(false) // prevent user to close dialog window by system back button
         //button which closes the dialog window - start
         val btnclose = dialog.findViewById<TextView>(R.id.btnclose)
@@ -79,7 +87,7 @@ class Level1 : AppCompatActivity() {
 
         //button "Continue" - start
         val btncontinue = dialog.findViewById<Button>(R.id.btncontinue)
-        btncontinue.setOnClickListener{
+        btncontinue.setOnClickListener {
             dialog.dismiss() //closing dialog window
         }
         //button "Continue" - end
@@ -93,13 +101,30 @@ class Level1 : AppCompatActivity() {
 
 
         numRight = (0 until 10).random()
-        while(numLeft==numRight) {
+        while (numLeft == numRight) {
             numRight = (0 until 10).random()
         }
         findViewById<ImageView>(R.id.image_right).setImageResource(Array.images1[numRight])
         findViewById<TextView>(R.id.text_right).setText(Array.texts1[numRight])
 
+        //making left picture clickable - start
+        findViewById<ImageView>(R.id.image_left).setOnTouchListener { view, motionEvent ->
+           if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                findViewById<ImageView>(R.id.image_right).setEnabled(false)
+                if (numLeft > numRight) {
+                    findViewById<ImageView>(R.id.image_left).setImageResource(R.drawable.img_true)
+                } else {
+                    findViewById<ImageView>(R.id.image_left).setImageResource(R.drawable.img_false)
+                }
+            } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
 
+            }
+            //return@setOnTouchListener true
+            true // as in https://stuff.mit.edu/afs/sipb/project/android/docs/reference/android/view/MotionEvent.html
+        }
+
+
+        //making left picture clickable - end
 
 
     }
@@ -115,4 +140,8 @@ class Level1 : AppCompatActivity() {
         }
     }
     //system back button changed - end
+
+
 }
+
+
